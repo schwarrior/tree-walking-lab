0408b3c3f03b690d01d2e485f1de81beda96c656
Robert Schwartz <robsmicrosoft@prettyapps.net>
2021-06-12 16:05:58 -0400

    sample objects

--- src/robs-tree-walker.ts
+++ src/robs-tree-walker.ts
@@ -1,6 +1,21 @@
+const carCollections: object = {
+    'RobsCars': [
+        {'Make':'Ford', 'Model': 'F150', 'Year': '2011', 'PreviousOwners': ['Carl']}
+        ,{'Make':'Jeep', 'Model': 'Wrangler', 'Year': '2015', 'PreviousOwners': ['Amanda', 'Larry']}
+    ],
+    'JerrysCars': [
+        {'Make':'BMW', 'Model': 'M3', 'Year': '2016', 'PreviousOwners': []}
+        ,{'Make':'Lexus', 'Model': 'LS400', 'Year': '2014', 'PreviousOwners': ['Charles', 'Tom', 'Greg']}
+    ],
+    'JennysCars': [
+        {'Make':'Subaru', 'Model': 'WRX', 'Year': '2009', 'PreviousOwners': ['Will', 'John', 'Jerry']}
+        ,{'Make':'Chevy', 'Model': 'Camaro', 'Year': '2017', 'PreviousOwners': ['Amanda']}
+    ]
+}
+
 export class RobsTreeWalker {
 
-    walk = (): void => {
+    walk = (treeObj: object): void => {
         console.log('starting walk')
     }
 
