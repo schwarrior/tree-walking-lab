7709c457f9d5dce3bb09e2b62646b2fe128a5bf7
Robert Schwartz <robsmicrosoft@prettyapps.net>
2021-06-14 14:11:29 -0400

    implement ElementAction

--- build/robs-tree-walker.js
+++ build/robs-tree-walker.js
@@ -4,53 +4,69 @@ exports.RobsTreeWalker = void 0;
 class RobsTreeWalker {
     constructor() {
         this.walk = (obj) => {
+            this.stack = new Array();
             console.log('Starting walk');
-            this.walkElement(null, obj);
+            const rootElem = { key: null, value: obj, parent: null };
+            this.walkElement(rootElem, this.stackElement);
             console.log('Walk complete');
+            console.log('');
+            console.log('Stack values:');
+            console.dir(this.stack);
         };
-        this.walkElement = (key, value) => {
-            if (this.isPrimitive(value)) {
-                if (!key) {
-                    console.log(`Element is not an object or an array. Single value found: '${value}'`);
+        this.stack = [];
+        this.stackElement = (elem) => {
+            this.stack.push(elem);
+        };
+        this.walkElement = (elem, elementAction) => {
+            elementAction(elem);
+            if (this.isPrimitive(elem.value)) {
+                if (!elem.key) {
+                    console.log(`Element is not an object or an array. Single value found: '${elem.value}'`);
                 }
                 else {
-                    console.log(`Leaf found. '${key}': '${value}'`);
+                    console.log(`Leaf found. '${elem.key}': '${elem.value}'`);
                 }
             }
-            else if (Array.isArray(value)) {
-                const ar = value;
-                if (!key) {
+            else if (Array.isArray(elem.value)) {
+                const ar = elem.value;
+                if (!elem.key) {
                     console.log('Walking from element root. Found array.');
                 }
                 else {
-                    console.log(`Walking into array with key: '${key}'`);
+                    console.log(`Walking into array with key: '${elem.key}'`);
                 }
                 for (let idx in ar) {
-                    const propAlias = `${key}.${idx}`;
-                    const memberElem = ar[idx];
-                    this.walkElement(propAlias, memberElem);
+                    const propAlias = `${elem.key}.${idx}`;
+                    const memberVal = ar[idx];
+                    const childElem = { key: propAlias, value: memberVal, parent: ar };
+                    this.walkElement(childElem, elementAction);
                 }
             }
             else {
                 // value must be an object
-                const obj = value;
-                if (!key) {
+                const obj = elem.value;
+                if (!elem.key) {
                     console.log('Walking from element root. Found to be an object.');
                 }
                 else {
-                    console.log(`Walking into element object with key: '${key}'`);
+                    console.log(`Walking into element object with key: '${elem.key}'`);
                 }
                 for (let prop in obj) {
                     if (!Object.prototype.hasOwnProperty.call(obj, prop)) {
                         continue;
                     }
-                    const childElem = obj[prop];
-                    this.walkElement(prop, childElem);
+                    const childVal = obj[prop];
+                    const childElem = { key: prop, value: childVal, parent: obj };
+                    this.walkElement(childElem, elementAction);
                 }
             }
         };
         this.isPrimitive = (test) => {
             return test !== Object(test);
+        };
+        this.isString = (test) => {
+            const isStr = (typeof test === 'string' || test instanceof String);
+            return isStr;
         };
     }
 }
--- src/robs-tree-walker.ts
+++ src/robs-tree-walker.ts
@@ -1,48 +1,73 @@
+interface Element {
+    key: string | null
+    value: any
+    parent: any
+}
+
 export class RobsTreeWalker {
 
     walk = (obj: object): void => {
+        this.stack = new Array<Element>()
         console.log('Starting walk')
-        this.walkElement(null, obj)
+        const rootElem: Element = {key: null, value: obj, parent: null}
+        this.walkElement(rootElem, this.stackElement)
         console.log('Walk complete')
+        console.log('')
+        console.log('Stack values:')
+        console.dir(this.stack)
     }
 
-    private walkElement = (key: string | null, value: any): void => {
-        if (this.isPrimitive(value)) {
-            if (!key) {
-                console.log(`Element is not an object or an array. Single value found: '${value}'`)
+    private stack: Array<Element> = []
+
+    private stackElement = (elem: Element) : void => {
+        this.stack.push(elem)
+    } 
+
+    private walkElement = (elem: Element, elementAction: {(elem: Element) : void}): void => {
+        elementAction(elem)
+        if (this.isPrimitive(elem.value)) {
+            if (!elem.key) {
+                console.log(`Element is not an object or an array. Single value found: '${elem.value}'`)
             } else {
-                console.log(`Leaf found. '${key}': '${value}'`)
+                console.log(`Leaf found. '${elem.key}': '${elem.value}'`)
             }
-        } else if (Array.isArray(value)) {
-            const ar = value;
-            if (!key){
+        } else if (Array.isArray(elem.value)) {
+            const ar = elem.value as []
+            if (!elem.key){
                 console.log('Walking from element root. Found array.')
             } else {
-                console.log(`Walking into array with key: '${key}'`)
+                console.log(`Walking into array with key: '${elem.key}'`)
             }
             for (let idx in ar) {
-                const propAlias = `${key}.${idx}`
-                const memberElem = ar[idx]
-                this.walkElement(propAlias, memberElem)
+                const propAlias = `${elem.key}.${idx}`
+                const memberVal = ar[idx]
+                const childElem: Element = {key: propAlias, value: memberVal, parent: ar}
+                this.walkElement(childElem, elementAction)
             }
         } else {
             // value must be an object
-            const obj = value
-            if (!key){
+            const obj = elem.value
+            if (!elem.key){
                 console.log('Walking from element root. Found to be an object.')
             } else {
-                console.log(`Walking into element object with key: '${key}'`)
+                console.log(`Walking into element object with key: '${elem.key}'`)
             }
             for (let prop in obj) {
                 if (!Object.prototype.hasOwnProperty.call(obj, prop)) { continue }
-                const childElem = obj[prop]
-                this.walkElement(prop, childElem)
+                const childVal = obj[prop]
+                const childElem: Element = {key: prop, value: childVal, parent: obj}
+                this.walkElement(childElem, elementAction)
             }
         }
     }
 
     private isPrimitive = (test: any): boolean => {
-        return test !== Object(test);
+        return test !== Object(test)
+    }
+
+    private isString = (test: any): boolean => {
+        const isStr = (typeof test === 'string' || test instanceof String)
+        return isStr
     }
 
 }
